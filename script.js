098b0c5707ae646d56e5156cbfb6c59cea7df4d9
Curtis Lacy <curtis@engine.co>
2013-07-17 09:32:34 -0400

    Original Code as posted at http://www.mediafire.com/download/qdyi4fksiegep24/sidebar.zip

---
+++ script.js
@@ -0,0 +1,41 @@
+/*Handle requests from background.html*/
+function handleRequest(
+	//The object data with the request params
+	request, 
+	//These last two ones isn't important for this example, if you want know more about it visit: http://code.google.com/chrome/extensions/messaging.html
+	sender, sendResponse
+	) {
+	if (request.callFunction == "toggleSidebar")
+		toggleSidebar();
+}
+chrome.extension.onRequest.addListener(handleRequest);
+
+/*Small function wich create a sidebar(just to illustrate my point)*/
+var sidebarOpen = false;
+function toggleSidebar() {
+	if(sidebarOpen) {
+		var el = document.getElementById('mySidebar');
+		el.parentNode.removeChild(el);
+		sidebarOpen = false;
+	}
+	else {
+		var sidebar = document.createElement('div');
+		sidebar.id = "mySidebar";
+		sidebar.innerHTML = "\
+			<h1>Hello</h1>\
+			World!\
+		";
+		sidebar.style.cssText = "\
+			position:fixed;\
+			top:0px;\
+			left:0px;\
+			width:30%;\
+			height:100%;\
+			background:white;\
+			box-shadow:inset 0 0 1em black;\
+			z-index:999999;\
+		";
+		document.body.appendChild(sidebar);
+		sidebarOpen = true;
+	}
+}
